8c65996446f96cf659de7c75cc7d94eaaeea8196
Jakub Supa <jakub.supa@icloud.com>
2021-12-29 13:01:14 +0100

    dynamic timeout

--- app.js
+++ app.js
@@ -1,53 +1,58 @@
-var SteamUser = require("steam-user");
-var SteamTradeOfferManager = require("steam-tradeoffer-manager");
-var SteamCommunity = require("steamcommunity");
-var SteamTotp = require("steam-totp");
+const SteamUser = require('steam-user');
+const SteamTradeOfferManager = require('steam-tradeoffer-manager');
+const SteamCommunity = require('steamcommunity');
+const SteamTotp = require('steam-totp');
 
-var settings = require("./settings");
+const settings = require('./settings.json');
 
-let steamUser = {};
-let steamCommunity = {};
-let tradeManager = {};
+const steamUser = {};
+const steamCommunity = {};
+const tradeManager = {};
 
 settings.users.forEach((userSettings, userIndex) => {
-    steamUser[userIndex] = new SteamUser();
-    steamCommunity[userIndex] = new SteamCommunity();
+    setTimeout(() => {
+        steamUser[userIndex] = new SteamUser();
+        steamCommunity[userIndex] = new SteamCommunity();
 
-    tradeManager[userIndex] = new SteamTradeOfferManager({
-        steam: steamUser[userIndex],
-        language: "en",
-    });
+        tradeManager[userIndex] = new SteamTradeOfferManager({
+            steam: steamUser[userIndex],
+            language: 'en',
+        });
 
-    steamUser[userIndex].logOn({
-        accountName: userSettings.username,
-        password: userSettings.password,
-        twoFactorCode: SteamTotp.getAuthCode(userSettings.shared_secret),
-    });
+        steamUser[userIndex].logOn({
+            accountName: userSettings.username,
+            password: userSettings.password,
+            twoFactorCode: SteamTotp.getAuthCode(userSettings.shared_secret),
+        });
 
-    steamUser[userIndex].on("webSession", function (sessionID, cookies) {
-        steamCommunity[userIndex].setCookies(cookies);
-        tradeManager[userIndex].setCookies(cookies);
-        console.log("cookies set");
-    });
+        steamUser[userIndex].on('webSession', (sessionID, cookies) => {
+            steamCommunity[userIndex].setCookies(cookies);
+            tradeManager[userIndex].setCookies(cookies);
+            console.log('cookies set');
+        });
 
-    steamUser[userIndex].on("loggedOn", () => {
-        steamUser[userIndex].setPersona(0);
-        console.log("logged on");
-    });
+        steamUser[userIndex].on('loggedOn', () => {
+            steamUser[userIndex].setPersona(0);
+            console.log('logged on');
+        });
 
-    tradeManager[userIndex].on("newOffer", (offer) => {
-        if (offer.message == settings.trade_key) {
-            setTimeout(function () {
-                getOffer(offer, userIndex);
+        tradeManager[userIndex].on('newOffer', (offer) => {
+            setTimeout(() => {
+                if (offer.message === settings.trade_key) {
+                    getOffer(offer, userIndex);
+                }
             }, 10000);
-        }
-    });
+        });
+    }, userIndex * 1000 + 10000);
 });
 
 function getOffer(offer, userIndex) {
     offer.update((err) => {
         if (err) throw err;
-        if (offer.itemsToGive.length != 0 || offer.itemsToReceive.length != 1) {
+        if (
+            offer.itemsToGive.length !== 0 ||
+            offer.itemsToReceive.length !== 1
+        ) {
             offer.decline((err) => {
                 if (err) throw err;
             });
@@ -61,12 +66,12 @@ function getOffer(offer, userIndex) {
                 true,
                 (err, inventory) => {
                     if (err) throw err;
-                    for (var i = 0; i < inventory.length; i++) {
-                        let item = inventory[i];
-                        if (item.classid == offer.itemsToReceive[0].classid) {
-                            var newOffer = tradeManager[userIndex].createOffer(
-                                offer.partner
-                            );
+                    for (let i = 0; i < inventory.length; i++) {
+                        const item = inventory[i];
+                        if (item.classid === offer.itemsToReceive[0].classid) {
+                            const newOffer = tradeManager[
+                                userIndex
+                            ].createOffer(offer.partner);
                             newOffer.addMyItem(item);
                             newOffer.setMessage(settings.trade_key);
                             newOffer.send((err) => {
@@ -76,35 +81,35 @@ function getOffer(offer, userIndex) {
                             break;
                         }
                     }
-                }
+                },
             );
         });
     });
 }
 
 function checkConfirmation(id, userIndex) {
-    var time = getTime();
-    var identity = settings.users[userIndex].identity_secret;
-    var confKey = SteamTotp.getConfirmationKey(identity, time, "conf");
+    const time = getTime();
+    const identity = settings.users[userIndex].identity_secret;
+    const confKey = SteamTotp.getConfirmationKey(identity, time, 'conf');
     steamCommunity[userIndex].getConfirmations(
         time,
         confKey,
         (err, confirmations) => {
             if (err) throw err;
             confirmations.forEach((confirmation) => {
-                if (confirmation.creator == id) {
-                    var time = getTime();
-                    var confKey = SteamTotp.getConfirmationKey(
+                if (confirmation.creator === id) {
+                    const time = getTime();
+                    const confKey = SteamTotp.getConfirmationKey(
                         identity,
                         time,
-                        "allow"
+                        'allow',
                     );
                     confirmation.respond(time, confKey, true, (err) => {
                         if (err) throw err;
                     });
                 }
             });
-        }
+        },
     );
 }
 
